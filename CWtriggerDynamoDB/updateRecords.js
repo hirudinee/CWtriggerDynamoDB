d98f15c5d3887f6b096fb2959fce86d497c97a8b
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-07 17:32:10 +0530

    update table

---
+++ CWtriggerDynamoDB/updateRecords.js
@@ -0,0 +1,43 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	ddb.update({
+		TableName: 'TestHiru',
+		Key: {
+			'ID': '002'
+		},
+		ExpressionAttributeNames: {
+			'#id': 'ID'
+		},
+		ExpressionAttributeValues: {
+			':idnew': '123'
+		},
+		UpdateExpression: 'set #id = :idnew',
+		ConditionExpression: '#id < :idnew'
+	}, function (err, data) {
+		if (err) {
+			callback(null,err);
+			console.log('Error : ',err);
+		} else {
+			callback( null,data);
+			console.log('success : ',data);
+		}
+	});
+
+
+	//callback(null, 'Successfully executed');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
