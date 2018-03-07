77181fdfafa2915316a1437e57583d2d277324bd
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-07 15:37:34 +0530

    test

---
+++ CWtriggerDynamoDB/getRecords.js
@@ -0,0 +1,17 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.get({
+		TableName: 'TestHiru',
+		Key: { 'ID': 'ID' }
+	}, function (err, data) {
+		if (err) {
+			callback('error',err);
+		} else {
+			callback('Data',data);
+		}
+	});
+
+
+	callback(null, 'Successfully executed');
+}
